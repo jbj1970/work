cd995551fd86e831f7fe1f7da77be606bc4510ee
JBJ <jbj@cryptomathic.com>
2021-01-12 10:57:28 +0100

    Update README.md, add more robustness

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# work
-Files for installation testing
+# Work
+This project is for automatic inspection of installed files on a Windows system (file product names, file product versions, file copyrights, validation of file signatures etc.). 
+
+The goal is to be able to run a PowerShell script or command calling one or more PowerShell functions like:
+```
+<file path> | <PS function> | <PS function>
+```
+which will traverse and look for all installer files in `<file path>` and for each one install it in a docker image. Then to generate a report with all relevant information about the relevant files installed.
+
+## Prerequisites
+The prerequisites are:
+- Windows OS version compatible with docker image mcr.microsoft.com/windows/servercore:1909 (e.g. Windows OS Build 18363.1256)
+- Windows Powershell 7.1 (5.1 might work as well)
+
+## Development perspective
+- Assemble a docker image with a PowerShell inspection script running inside the docker container. The script will be invoked by the host
+- Traverse the specified `file path` on the host looking for installer files
+- Convenient functions (mostly related to docker) are put into a Powershell profile
--- hostfiles/testinstall.ps1
+++ hostfiles/testinstall.ps1
@@ -41,28 +41,59 @@ function Get-CryptomathicReport {
     
     process {
         Write-Host $InstallItem.Name
+        
         $containerId = docker run -it -d $ENV:IMAGETAGCKMS
+        if($LastExitCode -eq 1) {
+            # simply continue with next item in pipeline
+            Write-Error "Could not start container on image $ENV:IMAGETAGCKMS"
+            return
+        }
 
         $exeFile = $InstallItem.Name
-        $fileName = ".\$exeFile.txt"
-        $hostFile = $InstallItem.FullName
-        $exeFilePath = "C:\$exeFile"
-        $containerFile = $containerId+":$exeFilePath"
+        try {
+            $fileName = ".\$exeFile.txt"
+            $hostFile = $InstallItem.FullName
+            $exeFilePath = "C:\$exeFile"
+            $containerFile = $containerId+":$exeFilePath"
+            
+            docker stop $containerId > $null
+            docker cp $hostFile $containerFile
+            docker start $containerId > $null
 
-        docker stop $containerId
-        docker cp $hostFile $containerFile
-        
-        docker start $containerId
-        docker exec $containerId $exeFilePath /q APPDIR="C:\Installation"
-        $res = docker exec -e SIGNERTHUMBPRINT=$Thumbprint $containerId "powershell" -File "c:\report.ps1"
-        docker kill $containerId
-        
-        $res | Format-List | Out-File $fileName
+            docker exec $containerId $exeFilePath /q APPDIR="C:\Installation"
+
+            # TODO: Fix this
+            # $installationJob = Start-Job -ScriptBlock {
+            #     Invoke-Command -ComputerName $env:COMPUTERNAME -ScriptBlock {
+            #         docker exec $containerId $exeFilePath /q APPDIR="C:\Installation"
+            #     } 
+            # }
+            # $installationJob | Wait-Job -Timeout 180
+            # if ($installationJob.state -eq 'Running') {
+            #     # simply continue with next item in pipeline
+            #     Write-Error "Installation of $exeFile timed out after 180 seconds"
+            #     return
+            # }
+
+            $res = docker exec -e SIGNERTHUMBPRINT=$Thumbprint $containerId "powershell" -File "c:\report.ps1"
+            if([string]::IsNullOrEmpty($res)) {
+                # simply continue with next item in pipeline
+                Write-Error "Could not inspect installed files"
+                return
+            }
+            $res | Format-List | Out-File $fileName
+            
+            #docker exec -e SIGNERTHUMBPRINT=$Thumbprint $containerId "powershell" -File "c:\report.ps1" | Format-List | Out-File $fileName
+        } catch {
+            # simply continue with next item in pipeline
+            Write-Error "Could not inspect installed files for $exeFile"
+        } finally {
+            docker kill $containerId > $null
+        }
     }
     
     end {
     }
 }
 
-#$args[0] | Get-CryptomathicMsi | ForEach-Object { Get-CryptomathicReport -InstallItem $_ -Thumbprint "bbc51542eba868c49c5458e9eef556d0d54c2463" }
-"C:\docker\ckms\1.20.0 RC1\Addons\Listeners" | Get-CryptomathicMsi | ForEach-Object { Get-CryptomathicReport -InstallItem $_ -Thumbprint "bbc51542eba868c49c5458e9eef556d0d54c2463" }
+$args[0] | Get-CryptomathicMsi | ForEach-Object { Get-CryptomathicReport -InstallItem $_ -Thumbprint "bbc51542eba868c49c5458e9eef556d0d54c2463" }
